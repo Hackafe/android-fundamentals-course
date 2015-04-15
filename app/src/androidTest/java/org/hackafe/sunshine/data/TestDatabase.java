5431945b19e7eaafca60b6bfaeca6a526db09144
Geno Roupsky <geno@roupsky.name>
2015-04-15 20:06:15 +0300

    unfinished test for testing multithreading of sqlite database

---
+++ app/src/androidTest/java/org/hackafe/sunshine/data/TestDatabase.java
@@ -0,0 +1,41 @@
+package org.hackafe.sunshine.data;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+/**
+ * Created by groupsky on 15.04.15.
+ */
+public class TestDatabase extends AndroidTestCase {
+
+    public void testDatabaseCanOpenWritableWhileOpenReadable() throws Exception {
+
+        final WeatherDbHelper helper = new WeatherDbHelper(getContext());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getId()+": will open for reading");
+                SQLiteDatabase readDb = helper.getReadableDatabase();
+                System.out.println(Thread.currentThread().getId()+": opened for reading");
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                readDb.close();
+                System.out.println(Thread.currentThread().getId()+": closed for reading");
+            }
+        }).run();
+//        Thread.sleep(1000);
+        System.out.println(Thread.currentThread().getId()+": will open for writing");
+        SQLiteDatabase writeDb = helper.getWritableDatabase();
+        System.out.println(Thread.currentThread().getId()+": opened for writing");
+        writeDb.close();
+        System.out.println(Thread.currentThread().getId()+": closed for writing");
+
+        // TODO check that database can be opened for writing while there is open for reading and vice verse
+    }
+
+}
---
+++ app/src/main/java/org/hackafe/sunshine/data/WeatherDbHelper.java
@@ -0,0 +1,28 @@
+package org.hackafe.sunshine.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by groupsky on 15.04.15.
+ */
+public class WeatherDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "weather.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public WeatherDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
