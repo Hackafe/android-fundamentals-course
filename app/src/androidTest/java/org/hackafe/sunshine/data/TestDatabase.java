5c9f4ac3df8fcf7e847ac012943b65a96644d438
Geno Roupsky <geno@roupsky.name>
2015-04-15 20:14:21 +0300

    test for database exists

--- app/src/androidTest/java/org/hackafe/sunshine/data/TestDatabase.java
+++ app/src/androidTest/java/org/hackafe/sunshine/data/TestDatabase.java
@@ -9,33 +9,53 @@ import android.util.Log;
  */
 public class TestDatabase extends AndroidTestCase {
 
-    public void testDatabaseCanOpenWritableWhileOpenReadable() throws Exception {
+    WeatherDbHelper helper;
 
-        final WeatherDbHelper helper = new WeatherDbHelper(getContext());
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        helper = new WeatherDbHelper(getContext());
+    }
+
+    public void _testDatabaseCanOpenWritableWhileOpenReadable() throws Exception {
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getId()+": will open for reading");
+                System.out.println(Thread.currentThread().getId() + ": will open for reading");
                 SQLiteDatabase readDb = helper.getReadableDatabase();
-                System.out.println(Thread.currentThread().getId()+": opened for reading");
+                System.out.println(Thread.currentThread().getId() + ": opened for reading");
                 try {
                     Thread.sleep(10000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 readDb.close();
-                System.out.println(Thread.currentThread().getId()+": closed for reading");
+                System.out.println(Thread.currentThread().getId() + ": closed for reading");
             }
         }).run();
 //        Thread.sleep(1000);
-        System.out.println(Thread.currentThread().getId()+": will open for writing");
+        System.out.println(Thread.currentThread().getId() + ": will open for writing");
         SQLiteDatabase writeDb = helper.getWritableDatabase();
-        System.out.println(Thread.currentThread().getId()+": opened for writing");
+        System.out.println(Thread.currentThread().getId() + ": opened for writing");
         writeDb.close();
-        System.out.println(Thread.currentThread().getId()+": closed for writing");
+        System.out.println(Thread.currentThread().getId() + ": closed for writing");
 
         // TODO check that database can be opened for writing while there is open for reading and vice verse
     }
 
+    public void testHelperIsNotNull() throws Exception {
+        assertNotNull(helper);
+    }
+
+    public void testDatabaseExists() throws Exception {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        assertNotNull(db);
+
+        assertEquals(WeatherDbHelper.DATABASE_VERSION, db.getVersion());
+    }
+
+    public void testTablesExists() throws Exception {
+
+    }
 }
--- app/src/main/java/org/hackafe/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/org/hackafe/sunshine/data/WeatherDbHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "weather.db";
-    private static final int DATABASE_VERSION = 1;
+    static final int DATABASE_VERSION = 1;
 
     public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
