bfc431f23edcb3b4002427bb6afe4a4bf8dadd7b
Geno Roupsky <geno@roupsky.name>
2015-04-29 19:46:20 +0300

    created cursor adapter and loaded data from contentprovider using CursorLoader

--- app/src/main/java/org/hackafe/sunshine/ForecastFragment.java
+++ app/src/main/java/org/hackafe/sunshine/ForecastFragment.java
@@ -3,11 +3,15 @@ package org.hackafe.sunshine;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +19,11 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import org.hackafe.sunshine.data.WeatherContract;
 import org.json.JSONArray;
@@ -30,15 +37,17 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import static org.hackafe.sunshine.data.WeatherContract.*;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ForecastFragment extends Fragment {
+public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final String TAG = "ForecastFragment";
     SharedPreferences mSharedPreferences;
     String prefUnits;
+    private SimpleCursorAdapter adapter;
 
     public ForecastFragment() {
     }
@@ -59,7 +68,7 @@ public class ForecastFragment extends Fragment {
         String data = getForecast();
         List<Forecast> forecasts = parseForecast(data);
 
-        for (Forecast forecast: forecasts) {
+        for (Forecast forecast : forecasts) {
             ContentValues values = new ContentValues();
             values.put(WeatherContract.Forecast.COLUMN_DATE, forecast.timestamp);
             values.put(WeatherContract.Forecast.COLUMN_FORECAST, forecast.desc);
@@ -68,11 +77,19 @@ public class ForecastFragment extends Fragment {
                     WeatherContract.Forecast.CONTENT_URI,
                     values
             );
-            Log.d(TAG, "added weather with uri: "+row);
+            Log.d(TAG, "added weather with uri: " + row);
         }
 
 
-        final ForecastAdapter adapter = new ForecastAdapter(inflater, forecasts);
+        adapter = new SimpleCursorAdapter(getActivity(),
+                R.layout.list_item_forecast,
+                null,
+                new String[]{
+                        WeatherContract.Forecast.COLUMN_FORECAST
+                },
+                new int[]{
+                        R.id.list_item_forecast_listview
+                });
         final ListView collection = (ListView) rootView.findViewById(R.id.container);
         collection.setAdapter(adapter);
 
@@ -93,6 +110,7 @@ public class ForecastFragment extends Fragment {
 
         Button addMoreBtn = (Button) rootView.findViewById(R.id.btn_add_more_items);
 
+        getLoaderManager().initLoader(1, null, this);
 
         return rootView;
     }
@@ -167,5 +185,36 @@ public class ForecastFragment extends Fragment {
             Log.e("Sunshine", t.getMessage(), t);
             return null;
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(
+                // Context context,
+                getActivity(),
+                // Uri uri,
+                WeatherContract.Forecast.CONTENT_URI,
+                // String[] projection,
+                new String[]{
+                        WeatherContract.Forecast._ID,
+                        WeatherContract.Forecast.COLUMN_FORECAST
+                },
+                // String selection,
+                null,
+                // String[] selectionArgs,
+                null,
+                // String sortOrder
+                WeatherContract.Forecast.COLUMN_DATE
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
