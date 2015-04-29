a1b499eb3070602e32dbdff9715982211bc5bc8e
Geno Roupsky <geno@roupsky.name>
2015-04-29 19:19:30 +0300

    test class for testing weather list

---
+++ app/src/androidTest/java/org/hackafe/sunshine/ui/TestWeatherList.java
@@ -0,0 +1,19 @@
+package org.hackafe.sunshine.ui;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.hackafe.sunshine.MainActivity;
+
+/**
+ * Created by groupsky on 29.04.15.
+ */
+public class TestWeatherList extends ActivityInstrumentationTestCase2 {
+
+    public TestWeatherList() {
+        super(MainActivity.class);
+    }
+
+    public void testFailure() {
+        assertTrue(false);
+    }
+}
